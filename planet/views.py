74a8b022762fe7e69cae772b5ff2239358d241a0
Ana Rey <anarey@gmail.com>
2012-05-29 00:42:47 +0200

    vista con la lista de post activos

--- planet/views.py
+++ planet/views.py
@@ -1,8 +1,15 @@
 # Create your views here.
-
+from planet.models import Blog
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Pagina del Planet")
+    blog_list = Blog.objects.all() ## TODO solo los activos
+#    enable_list = ', '.join([b.name for b in blog_list])
+    enable_list = []
+    for b in blog_list:
+        if b.enable:
+            enable_list.append(b.name)
+    return HttpResponse(enable_list)
+
 #def detail(request,blog_id):
 #    return HttpResponse("Details")
