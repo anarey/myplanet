04d0b33d095a5d3db2bd6200ea57902265e9a064
Ana Rey <anarey@gmail.com>
2012-05-28 23:52:15 +0200

    Modelo de datos blog y activa la zona de administración

---
+++ planet/admin.py
@@ -0,0 +1,4 @@
+from planet.models import Blog
+from django.contrib import admin
+
+admin.site.register(Blog)
--- planet/models.py
+++ planet/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    author = models.CharField(max_length = 100)
+    name = models.CharField(max_length = 100)
+    feed = models.URLField()
+    enable = models.BooleanField()
+
+    def __unicode__(self):
+        return self.name
+
