e02021dda7a0186d224a4017f0249bf4cde5bf17
Ana Rey <anarey@gmail.com>
2012-05-30 18:01:55 +0200

    cambio de la estructura a almacenar los post a un diccionario, ordenado los post por la fecha de publicacion. Muestra del contenido de los post en la vista principal

--- planet/views.py
+++ planet/views.py
@@ -18,14 +18,22 @@ def index(request):
 #            enable_list.append(b.feed)
             enable_list.append(b)
             d = feedparser.parse(b.feed)
+#            lista=[]
             for p in d['entries']:
-                post = []
+               # post = []
                 date = calendar.timegm(p.published_parsed)
-                post = [p.title, p.link, p.published_parsed,date, p.content[0].value]
-                post_list.append(post)
-            
+               # post = [p.title, p.link, p.published_parsed,date, p.content[0].value]
+               # post_list.append(post)
+                post_list.append({'date':date,'title':p.title,'date_format':p.published,'content':p.content[0].value,'link':p.link,'author':b.author})
+            list_aux = [(dict_["date"], dict_) for dict_ in post_list]
+            list_aux.sort(reverse=True)
+            order_post = [dist_ for (key,dist_) in list_aux]
 #    return HttpResponse(enable_list)
-    return render_to_response('planet/index.html',{'enable_list' : enable_list,'post_list':post_list})
+    return render_to_response('planet/index.html',{'enable_list' : enable_list,'post_list':order_post})
+
+
+##### Cambiando el nombre de las listas para mostrar los disccionarios.
+
 
 #def detail(request,blog_id):
 #    return HttpResponse("Details")
