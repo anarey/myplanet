73e1250a614ce235e2f58ac5b59afdf211c8ba9f
Ana Rey <anarey@gmail.com>
2012-05-30 13:25:44 +0200

    Cambio de estructura de tupla a lista para almacenar temporalmente los post

--- planet/views.py
+++ planet/views.py
@@ -1,22 +1,31 @@
 # Create your views here.
 import feedparser
 from planet.models import Blog
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
+#from time import mktime
+#from datetime import datetime
+import calendar
 
 def index(request):
-    blog_list = Blog.objects.all() ## TODO solo los activos
-#    enable_list = ', '.join([b.name for b in blog_list])
+    blog_list = Blog.objects.all() ## Todos los blogs
     enable_list = []
+    post_list = []
     for b in blog_list:
-        if b.enable:
-            enable_list.append(b.feed)
+        if b.enable:  ##por cada feed activo
+            i=0
+#            enable_list.append(b.feed)
+            enable_list.append(b)
             d = feedparser.parse(b.feed)
-            enable_list.append(d.entries[0].title)
-            enable_list.append(d.entries[0].link)
-            enable_list.append(d.entries[0].published)
-            enable_list.append(d.entries[0].content)
-    return HttpResponse(enable_list)
+            for p in d['entries']:
+                post = []
+                date = calendar.timegm(p.published_parsed)
+                post = [p.title, p.link, p.published_parsed,date, p.content[0].value]
+                post_list.append(post)
+            
+#    return HttpResponse(enable_list)
+    return render_to_response('planet/index.html',{'enable_list' : enable_list,'post_list':post_list})
 
 #def detail(request,blog_id):
 #    return HttpResponse("Details")
