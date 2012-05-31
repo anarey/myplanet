e7edd3d5061212986d19cc4681511a848d058543
Ana Rey <anarey@gmail.com>
2012-05-31 23:39:57 +0200

    Modificada pequeñas erratas en textos

--- planet/views.py
+++ planet/views.py
@@ -16,15 +16,10 @@ def index(request):
     for b in blog_list:
         if b.enable:  ##por cada feed activo
             i=0
-#            enable_list.append(b.feed)
             enable_list.append(b)
             d = feedparser.parse(b.feed)
-#            lista=[]
             for p in d['entries']:
-               # post = []
                 date = calendar.timegm(p.published_parsed)
-               # post = [p.title, p.link, p.published_parsed,date, p.content[0].value]
-               # post_list.append(post)
                 post_list.append({'date':date,'title':p.title,'date_format':p.published,'content':p.content[0].value,'link':p.link,'author':b.author})
             list_aux = [(dict_["date"], dict_) for dict_ in post_list]
             list_aux.sort(reverse=True)
@@ -40,12 +35,5 @@ def index(request):
             except EmptyPage:
             # If page is out of range (e.g. 9999), deliver last page of results.
                 posts = paginator.page(paginator.num_pages)
-#    return HttpResponse(enable_list)
     return render_to_response('index.html',{'enable_list' : enable_list,'post_list':posts})
 
-
-##### Cambiando el nombre de las listas para mostrar los disccionarios.
-
-
-#def detail(request,blog_id):
-#    return HttpResponse("Details")
