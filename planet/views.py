4a046c7129352b8e3037b9ccd515cd3ed4320769
Ana Rey <anarey@gmail.com>
2012-06-01 20:19:17 +0200

    Eliminado una estructura condicional, a cambio de recibir sólo los objetos que cumplen una condición concreta, a través de la QuerySet API

--- planet/views.py
+++ planet/views.py
@@ -10,30 +10,33 @@ import calendar
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
-    blog_list = Blog.objects.all() ## Todos los blogs
+#    blog_list = Blog.objects.all() ## Todos los blogs
+    ## Obtemos solo los objetos que cumplen dicha condicion
+    ## Que estan activos
+    blog_list = Blog.objects.filter(enable=True) 
     enable_list = []
     post_list = []
     for b in blog_list:
-        if b.enable:  ##por cada feed activo
-            i=0
-            enable_list.append(b)
-            d = feedparser.parse(b.feed)
-            for p in d['entries']:
-                date = calendar.timegm(p.published_parsed)
-                post_list.append({'date':date,'title':p.title,'date_format':p.published,'content':p.content[0].value,'link':p.link,'author':b.author})
-            list_aux = [(dict_["date"], dict_) for dict_ in post_list]
-            list_aux.sort(reverse=True)
-            order_post = [dist_ for (key,dist_) in list_aux]
+#        if b.enable:  ##por cada feed activo
+        i=0
+        enable_list.append(b)
+        d = feedparser.parse(b.feed)
+        for p in d['entries']:
+            date = calendar.timegm(p.published_parsed)
+            post_list.append({'date':date,'title':p.title,'date_format':p.published,'content':p.content[0].value,'link':p.link,'author':b.author})
+        list_aux = [(dict_["date"], dict_) for dict_ in post_list]
+        list_aux.sort(reverse=True)
+        order_post = [dist_ for (key,dist_) in list_aux]
 
-            paginator = Paginator(order_post,6)
-            page = request.GET.get('page')
-            try:
-                posts = paginator.page(page)
-            except PageNotAnInteger:
-            # If page is not an integer, deliver first page.
-                posts = paginator.page(1)
-            except EmptyPage:
-            # If page is out of range (e.g. 9999), deliver last page of results.
-                posts = paginator.page(paginator.num_pages)
+        paginator = Paginator(order_post,6)
+        page = request.GET.get('page')
+        try:
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+           # If page is not an integer, deliver first page.
+            posts = paginator.page(1)
+        except EmptyPage:
+           # If page is out of range (e.g. 9999), deliver last page of results.
+            posts = paginator.page(paginator.num_pages)
     return render_to_response('index.html',{'enable_list' : enable_list,'post_list':posts})
 
