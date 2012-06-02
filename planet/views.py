4f390d75e8345c67d374678c641484edadf16b36
Ana Rey <anarey@gmail.com>
2012-06-02 17:14:05 +0200

    Mejora del codigo. Sacada la ordenación y paginación del bucle principal, además de un cambio relacionado con filter

--- planet/views.py
+++ planet/views.py
@@ -13,30 +13,28 @@ def index(request):
 #    blog_list = Blog.objects.all() ## Todos los blogs
     ## Obtemos solo los objetos que cumplen dicha condicion
     ## Que estan activos
-    blog_list = Blog.objects.filter(enable=True) 
-    enable_list = []
+    #blog_list = Blog.objects.filter(enable=True) 
+    enable_list = Blog.objects.filter(enable = True)
+    #enable_list = []
     post_list = []
-    for b in blog_list:
-#        if b.enable:  ##por cada feed activo
-        i=0
-        enable_list.append(b)
+    for b in enable_list:
         d = feedparser.parse(b.feed)
         for p in d['entries']:
             date = calendar.timegm(p.published_parsed)
             post_list.append({'date':date,'title':p.title,'date_format':p.published,'content':p.content[0].value,'link':p.link,'author':b.author})
-        list_aux = [(dict_["date"], dict_) for dict_ in post_list]
-        list_aux.sort(reverse=True)
-        order_post = [dist_ for (key,dist_) in list_aux]
+    list_aux = [(dict_["date"], dict_) for dict_ in post_list]
+    list_aux.sort(reverse=True)
+    order_post = [dist_ for (key,dist_) in list_aux]
 
-        paginator = Paginator(order_post,6)
-        page = request.GET.get('page')
-        try:
-            posts = paginator.page(page)
-        except PageNotAnInteger:
-           # If page is not an integer, deliver first page.
-            posts = paginator.page(1)
-        except EmptyPage:
-           # If page is out of range (e.g. 9999), deliver last page of results.
-            posts = paginator.page(paginator.num_pages)
+    paginator = Paginator(order_post,6)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+     # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+     # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
     return render_to_response('index.html',{'enable_list' : enable_list,'post_list':posts})
 
